63d9afb355460135017562a10a18badbc9004b39
Crow <Crow@Crow-PC>
2016-03-11 13:19:38 +0100

    Classifier inital logic

--- SpamClassifier/src/Classifier/NaiveBayes.java
+++ SpamClassifier/src/Classifier/NaiveBayes.java
@@ -12,12 +12,29 @@ public class NaiveBayes {
 	this.builder=builder;
 }
 	
-	public TYPE classify(String content){
+	private TYPE classify(EmailObject email){
 		return null;
 	}
 	
 	public void classifyByDatasetReader(DatasetReader reader){
-		List<EmailObject> e;
+		System.out.println("\n Classifying test emails..");
+		List<EmailObject> e_s=reader.getSpams_emails_test();
+		List<EmailObject> e_h=reader.getHams_emails_test();
+		int total_emails=e_s.size()+e_h.size();
+		int right_classified=0;
+		for(EmailObject e: e_s){
+			TYPE t=classify(e);
+			if(t==TYPE.SPAM)
+				right_classified++;
+		}
+		
+		for(EmailObject e: e_h){
+			TYPE t=classify(e);
+			if(t==TYPE.HAM)
+				right_classified++;
+		}
+	//	double precision=right_classified/total_emails;
+		
 	}
 	
 }
